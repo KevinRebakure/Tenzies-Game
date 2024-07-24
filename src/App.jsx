f1ba195d9d8da23aac2fbdd9c2fb585b8baf4fe2
Rebakure <kevinrebakure@gmail.com>
2024-07-24 13:04:08 +0200

    header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import Dice from "./components/Dice";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <>
       <div className="relative flex h-[100vh] w-full items-center justify-center bg-[#0B2434]">
         <div className="w-[640px] space-y-3 rounded-[20px] bg-[#F5F5F5] p-[30px]">
-          <h1 className="text-center text-4xl font-bold">Tenzies</h1>
-          <p className="mx-auto w-[300px] text-center">
-            Roll until all dice are the same. Click each die to freeze it at its
-            current value between rolls.
-          </p>
+          <Header />
           <div className="flex w-full flex-wrap gap-10 justify-center">
             <Dice hold={true} number={2}/>
             <Dice hold={false} number={4}/>
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+      <>
+        <h1 className="text-center text-4xl font-bold">Tenzies</h1>
+        <p className="mx-auto w-[300px] text-center">
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls.
+        </p>
+      </>
+    );
+}

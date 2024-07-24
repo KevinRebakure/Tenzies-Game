5c05ccea6366efd78496ecdf0db1c3f71b43f0fa
Rebakure <kevinrebakure@gmail.com>
2024-07-24 13:08:09 +0200

    Update Dice.jsx

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,3 +1,3 @@
 export default function Dice({hold, number}) {
-    return <p className={`${hold && 'bg-[#59E391] hover:bg-[#59E391]'} shadow-lg flex items-center justify-center size-[70px] rounded-md hover:bg-gray-300 transition-all duration-100 text-2xl font-semibold`}>{number}</p>;
+    return <p className={`${hold && 'bg-[#59E391] hover:bg-[#59E391]'} shadow-lg flex items-center justify-center size-[70px] rounded-md hover:bg-gray-300 transition-all duration-75 text-2xl font-semibold`}>{number}</p>;
 }

72abf10938d68cd9404dee6c8c7b01ff752aba80
Rebakure <kevinrebakure@gmail.com>
2024-07-24 15:56:32 +0200

    Update Dice.jsx

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -4,7 +4,7 @@ export default function Dice({ value, handleHold, inGame }) {
       onClick={() => {
         handleHold(value.id);
       }}
-      className={`${value.hold && "bg-[#59E391] hover:bg-[#59E391]"} ${!inGame && "bg-[#5035FF] text-white"} flex size-[70px] items-center justify-center rounded-md text-2xl font-semibold shadow-lg transition-all duration-75 hover:bg-gray-300`}
+      className={`${value.hold && "bg-[#59E391]"} ${inGame ? "hover:bg-[#59E391]" : "hover:bg-[#5035FF] bg-[#5035FF] text-white"} flex size-[70px] items-center justify-center rounded-md text-2xl font-semibold shadow-lg transition-all duration-75 hover:bg-gray-300`}
     >
       {value.num}
     </button>

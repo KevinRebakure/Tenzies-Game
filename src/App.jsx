8c5db104beb5969320eb2a150fbb7d4be5f0fa0e
Rebakure <kevinrebakure@gmail.com>
2024-07-24 12:52:07 +0200

    card background

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 export default function App() {
   return (
     <>
-      <div>
+      <div className="relative flex h-[100vh] w-full items-center justify-center bg-[#0B2434]">
+        <div className="w-[640px] rounded-[20px] bg-[#F5F5F5] p-[30px] space-y-3">
+          <h1 className="text-center text-4xl font-bold">Tenzies</h1>
+          <p className="text-center w-[300px] mx-auto">
+            Roll until all dice are the same. Click each die to freeze it at its
+            current value between rolls.
+          </p>
+        </div>
       </div>
     </>
-  )
+  );
 }

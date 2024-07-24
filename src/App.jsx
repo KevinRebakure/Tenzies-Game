7f9c76e2d1a3b180814052b46e6f5132cbc24c3a
Rebakure <kevinrebakure@gmail.com>
2024-07-24 13:06:37 +0200

    roll button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Dice from "./components/Dice";
 import Header from "./components/Header";
+import Roll from "./components/Roll";
 
 export default function App() {
   return (
     <>
       <div className="relative flex h-[100vh] w-full items-center justify-center bg-[#0B2434]">
-        <div className="w-[640px] space-y-3 rounded-[20px] bg-[#F5F5F5] p-[30px]">
+        <div className="space-y-10 w-[640px] rounded-[20px] bg-[#F5F5F5] p-[30px]">
           <Header />
           <div className="flex w-full flex-wrap gap-10 justify-center">
             <Dice hold={true} number={2}/>
@@ -19,6 +20,7 @@ export default function App() {
             <Dice hold={false} number={3}/>
             <Dice hold={true} number={2}/>
           </div>
+          <Roll />
         </div>
       </div>
     </>
---
+++ src/components/Roll.jsx
@@ -0,0 +1,3 @@
+export default function Roll() {
+    return <button className="mx-auto block bg-[#5035FF] text-white text-xl font-medium py-3 px-10 rounded-lg">Roll</button>;
+}

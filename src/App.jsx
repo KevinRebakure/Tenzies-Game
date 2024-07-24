5583be6a84d083e91986024a6a25b87812bf639d
Rebakure <kevinrebakure@gmail.com>
2024-07-24 15:33:42 +0200

    roll feature

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,50 @@ import Header from "./components/Header";
 import Roll from "./components/Roll";
 
 export default function App() {
-  const[values, setValues] = useState(generateValues()) 
-  
+  const [values, setValues] = useState(generateValues());
+
   function generateValues() {
     const _values = [];
 
     for (let i = 1; i <= 10; i++) {
-      _values.push(Math.floor(Math.random() * 6 + 1));
+      _values.push({
+        num: Math.floor(Math.random() * 6 + 1),
+        hold: false,
+        id: i,
+      });
     }
 
-    return _values
+    return _values;
   }
 
   function rollDice() {
-    setValues(generateValues())
+    setValues((prev) => {
+      return [...prev.map((die)=>{
+        if (die.hold) {
+          return die
+        } else {
+          return {
+            num: Math.floor(Math.random() * 6 + 1),
+            hold: false,
+            id: die.id,
+          };
+        }
+      })];
+    });
+  }
+
+  function handleHold(dieID) {
+    setValues((prev) => {
+      return prev.map((die) => {
+        if (die.id !== dieID && die.hold === false) {
+          return die;
+        } else {
+          return { ...die, hold: true };
+        }
+      });
+
+      // return [...generateValues(), prev.filter((die)=>die.id === dieID)]
+    });
   }
 
   console.log(values);
@@ -28,8 +58,8 @@ export default function App() {
         <div className="w-[640px] space-y-10 rounded-[20px] bg-[#F5F5F5] p-[30px]">
           <Header />
           <div className="flex w-full flex-wrap justify-center gap-10">
-            {values.map((number, index) => (
-              <Dice hold={false} number={number} key={index} />
+            {values.map((value) => (
+              <Dice value={value} key={value.id} handleHold={handleHold} />
             ))}
           </div>
           <Roll rollDice={rollDice} />
--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,9 +1,12 @@
-export default function Dice({ hold, number }) {
+export default function Dice({ value, handleHold }) {
   return (
     <button
-      className={`${hold && "bg-[#59E391] hover:bg-[#59E391]"} flex size-[70px] items-center justify-center rounded-md text-2xl font-semibold shadow-lg transition-all duration-75 hover:bg-gray-300`}
+      onClick={() => {
+        handleHold(value.id);
+      }}
+      className={`${value.hold && "bg-[#59E391] hover:bg-[#59E391]"} flex size-[70px] items-center justify-center rounded-md text-2xl font-semibold shadow-lg transition-all duration-75 hover:bg-gray-300`}
     >
-      {number}
+      {value.num}
     </button>
   );
 }

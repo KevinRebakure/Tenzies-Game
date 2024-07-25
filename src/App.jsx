424f7e9dbf3e6535617812814a0636e500c9a8b9
Rebakure <kevinrebakure@gmail.com>
2024-07-25 14:51:24 +0200

    added an end game confetti cool animation

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,29 @@ import { useEffect, useState } from "react";
 import Dice from "./components/Dice";
 import Header from "./components/Header";
 import Roll from "./components/Roll";
+import Confetti from "react-confetti";
 
 export default function App() {
   const [values, setValues] = useState(
     JSON.parse(localStorage.getItem("values")) || generateValues(),
   );
 
-  const [end, setEnd] = useState(false)
+  const [end, setEnd] = useState(false);
 
-  useEffect(()=>{
-    const allChecked = values.every((die)=>die.hold)
-    const unique = values[0].num 
-    const allSimilar = values.every((die)=>die.num === unique)
+  useEffect(() => {
+    const allChecked = values.every((die) => die.hold);
+    const unique = values[0].num;
+    const allSimilar = values.every((die) => die.num === unique);
     if (allChecked && allSimilar) {
-      setEnd(true)
-      console.log('game over')
-      console.log(end)
+      setEnd(true);
+      console.log("game over");
+      console.log(end);
     }
-  }, [values])
+  }, [values]);
 
   useEffect(() => {
     localStorage.setItem("values", JSON.stringify(values));
   }, [values]);
-
 
   function generateValues() {
     const _values = [];
@@ -58,8 +58,8 @@ export default function App() {
         ];
       });
     } else {
-      setValues(generateValues())
-      setEnd(false)
+      setValues(generateValues());
+      setEnd(false);
     }
   }
 
@@ -77,15 +77,21 @@ export default function App() {
 
   return (
     <>
+      {end && <Confetti width={window.innerWidth} height={window.innerHeight} gravity={0.2} numberOfPieces={300}/>}
       <div className="relative flex h-[100vh] w-full items-center justify-center bg-[#0B2434]">
         <div className="w-[640px] space-y-10 rounded-[20px] bg-[#F5F5F5] p-[30px]">
           <Header />
           <div className="flex w-full flex-wrap justify-center gap-10">
             {values.map((value) => (
-              <Dice value={value} key={value.id} handleHold={handleHold} gameOver={end}/>
+              <Dice
+                value={value}
+                key={value.id}
+                handleHold={handleHold}
+                gameOver={end}
+              />
             ))}
           </div>
-          <Roll rollDice={rollDice} gameOver={end}/>
+          <Roll rollDice={rollDice} gameOver={end} />
         </div>
       </div>
     </>
